b2ef6522240025868db9175af9e40b2515bc3934
Kon James Ayuen <nyiel37@gmail.com>
2025-11-23 13:55:14 +0200

    Update project files

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def predict():
     return render_template('index.html', error="Invalid file type")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
